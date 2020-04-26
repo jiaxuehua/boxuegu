014a5dbec028ef2358af9fd978a2843797572677
jiaxuehua <1750996349@qq.com>
2020-04-26 16:17:07 +0800

    作业

---
+++ app/src/main/java/com/hbtangxun/boxuegu/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hbtangxun.boxuegu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.hbtangxun.boxuegu.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
